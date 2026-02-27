e213f6a83c726ef8df52364b3181a8f4df3a47e0
CheraHamza <cherahamza1@gmail.com>
2026-02-28 00:26:14 +0100

    Refactor database connection pool to use consistent environment variable names

--- storage/pool.js
+++ storage/pool.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-	host: process.env.HOST,
-	user: process.env.USER,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
 	database: process.env.DATABASE,
-	password: process.env.PASSWORD,
-	port: process.env.PORT,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
 	ssl: {
 		rejectUnauthorized: true,
 		ca: process.env.CA_CERT,
